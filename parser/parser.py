c8f834133673e2ff913917ec3148f86e52ced1cc
LakshmiMMohan <lakshmigilshamohan@gmail.com>
2026-06-30 21:38:21 +0530

    Fix circular import between parser and table in parser

--- parser/parser.py
+++ parser/parser.py
@@ -1,26 +1,26 @@
 import pymupdf
-from .table import rect_table,parse_table1,parse_table2,parse_table3,parse_table4
+from .table import table_rect,parse_table1,parse_table2,parse_table3,parse_table4
 
-with pymupdf.open("icc-stmt.pdf") as doc:
+with pymupdf.open("parser/icc-stmt.pdf") as doc:
     page=doc[0]
     #1st table
     cord1_from=page.search_for("ACCOUNT DETAILS - INR")
     cord1_to=page.search_for("Statement of Transactions in Savings Account Number: 035701533681 in INR for the period November 01, 2016 - November 30, 2016")
-    rect1=rect_table(cord1_from,cord1_to)
-    table1_data=parse_table1(rect1)
+    rect1=table_rect(page,cord1_from,cord1_to)
+    table1_data=parse_table1(page,rect1)
     #2nd table
     cord2_from=page.search_for("Statement of Transactions in Savings Account Number: 035701533681 in INR for the period November 01, 2016 - November 30, 2016")
     cord2_to=page.search_for("Total:")
-    rect2=rect_table(cord2_from,cord2_to)
-    table2_data=parse_table2(rect2)
+    rect2=table_rect(page,cord2_from,cord2_to)
+    table2_data=parse_table2(page,rect2)
     #3rd table
     cord3_from=page.search_for("REWARD POINTS SUMMARY")
     cord3_to=page.search_for("To get current reward points balance")
-    rect3=rect_table(cord3_from,cord3_to)
-    table3_data=parse_table3(rect3)
+    rect3=table_rect(page,cord3_from,cord3_to)
+    table3_data=parse_table3(page,rect3)
     #4th table
     cord4_from=page.search_for("Account Related Other Information")
     cord4_to=page.search_for("* Nominee name is displayed only on specific consent of customer.")
-    rect4=rect_table(cord4_from,cord4_to)
-    table4_data=parse_table4(rect4)
+    rect4=table_rect(page,cord4_from,cord4_to)
+    table4_data=parse_table4(page,rect4)
 
--- parser/table.py
+++ parser/table.py
@@ -1,30 +1,30 @@
 import pymupdf,re
-import parser
 
 
-def rect_table(cord_from,cord_to):
+
+def table_rect(page,cord_from,cord_to):
     '''Selct particular portion of pdf using rectangle coordinates'''
     
     ry0=cord_from[0].y1
-    rx0=parser.page.rect.x0
-    rx1=parser.page.rect.x1
+    rx0=page.rect.x0
+    rx1=page.rect.x1
     ry1=cord_to[0].y0
     
     cr=pymupdf.Rect(rx0,ry0,rx1,ry1)
     return cr
 
-def parse_table1(rect1):
+def parse_table1(page,rect1):
 
-    text=parser.page.get_text(clip=rect1,sort=True)
+    text=page.get_text(clip=rect1,sort=True)
     lines=[line.strip() for line in text.splitlines() if line.strip()]
     headers=["ACCOUNT TYPE","A/C. BALANCE (I)","FIXED DEPOSITS (LINKED) BAL. (II)","TOTAL BALANCE (I+II)","NOMINATION"]
     values=re.split(r"\s{2,}",lines[1])
     contents=dict(zip(headers,values))
     return contents
 
-def parse_table2(rect2):
+def parse_table2(page,rect2):
     '''parse second table into a list of dictionary with keys DATE,MODE,PARTICULARS,DEPOSITS,WITHDRAWLS,BALANCE'''
-    ft=parser.page.find_tables(clip=rect2,strategy="text")
+    ft=page.find_tables(clip=rect2,strategy="text")
     table=ft.tables[0]
     contents=[]
     rows=table.extract()
@@ -35,8 +35,8 @@ def parse_table2(rect2):
         contents.append(dict(zip(headers,row)))
     return contents
 
-def parse_table3(rect3):
-    text=parser.page.get_text(clip=rect3,sort=True)
+def parse_table3(page,rect3):
+    text=page.get_text(clip=rect3,sort=True)
     lines=[line.strip() for line in text.splitlines() if line.strip()]
     values_list=re.split(r"\s{2,}",lines[4])
     values=[int(value) for value in values_list]
@@ -46,9 +46,9 @@ def parse_table3(rect3):
                         "POINTS BALANCE*":values[4]}
     return table3
 
-def parse_table4(rect4):
+def parse_table4(page,rect4):
     
-    text=parser.page.get_text(clip=rect4,sort=True)
+    text=page.get_text(clip=rect4,sort=True)
     mod_values=[x for x in text.split("\n") if x.strip()]  #strip invalid values
     fields=re.split(r"\s{2,}",mod_values[0].strip())
     values=re.split(r"\s{2,}",mod_values[1].strip())
